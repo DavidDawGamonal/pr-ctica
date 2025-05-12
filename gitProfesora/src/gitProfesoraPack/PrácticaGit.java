ad87828b43166bbdbbc5932ac8153a42f240919c
Elvis <david@ELVIS>
2025-05-12 17:30:05 +0200

    1a comisión

---
+++ gitProfesora/src/gitProfesoraPack/PrácticaGit.java
@@ -0,0 +1,13 @@
+package gitProfesoraPack;
+
+public class PrácticaGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo git");
+		
+		String git="GitHub";
+
+	}
+
+}
